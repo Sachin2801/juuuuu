2a0812841688ca819b7797d5aac165a5653f5cde
unknown <sjathar48@gmail.com>
2022-08-20 22:49:42 +0530

    commit

---
+++ S1.java
@@ -0,0 +1,20 @@
+package SP;
+
+import java.util.ArrayList;
+
+public class S1 {
+
+	public static void main(String[] args) {
+		ArrayList ar= new ArrayList();
+		ar.add("sachin");
+		ar.add(100);
+		ar.add(10.1);
+		
+		System.out.println(ar);
+		System.out.println("abc");
+		System.out.println("xyz");
+		
+
+	}
+
+}
---
+++ S11.java
@@ -0,0 +1,16 @@
+package SP;
+
+public class S11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a= 20;
+int b=45;
+System.out.println(a+" "+b);
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println(a+" "+b);
+	}
+
+}
